ba159cb14c3268c15a03872c81acadaad9d70d56
Shafko <mykola.shafranov@student.fmf.uni-lj.si>
2020-12-03 15:41:59 +0100

    Uvoženi podatki, ustvarjene tabele in delno očiščeni podatki (izbrisani odvečni stolpci)

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,45 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+library(rjson)
+library(dplyr)
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+data <- fromJSON(file="podatki/eng.json")
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
+tabela <- lapply(data$teamTableStats,
+                 . %>% lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # poskrbimo za manjkajoče podatke
+                   as.data.frame()) %>% bind_rows() %>%
+                   select(-tournamentId,-tournamentRegionId,-isOpta,-tournamentRegionCode,-regionCode,-teamId,-teamRegionName,-seasonName,-seasonId,-ranking)
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+data2 <- fromJSON(file="podatki/ita.json")
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+tabela2 <- lapply(data2$teamTableStats,
+                 . %>% lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # poskrbimo za manjkajoče podatke
+                   as.data.frame()) %>% bind_rows() %>%
+                   select(-tournamentId,-tournamentRegionId,-isOpta,-tournamentRegionCode,-regionCode,-teamId,-teamRegionName,-seasonName,-seasonId,-ranking)
+
+
+data3 <- fromJSON(file="podatki/spa.json")
+
+tabela3 <- lapply(data3$teamTableStats,
+                  . %>% lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # poskrbimo za manjkajoče podatke
+                    as.data.frame()) %>% bind_rows() %>%
+                    select(-tournamentId,-tournamentRegionId,-isOpta,-tournamentRegionCode,-regionCode,-teamId,-teamRegionName,-seasonName,-seasonId,-ranking)
+
+
+data4 <- fromJSON(file="podatki/ger.json")
+
+tabela4 <- lapply(data4$teamTableStats,
+                  . %>% lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # poskrbimo za manjkajoče podatke
+                    as.data.frame()) %>% bind_rows() %>%
+                    select(-tournamentId,-tournamentRegionId,-isOpta,-tournamentRegionCode,-regionCode,-teamId,-teamRegionName,-seasonName,-seasonId,-ranking)
+
+
+data5 <- fromJSON(file="podatki/fra.json")
+
+tabela5 <- lapply(data5$teamTableStats,
+                  . %>% lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # poskrbimo za manjkajoče podatke
+                    as.data.frame()) %>% bind_rows() %>%
+                    select(-tournamentId,-tournamentRegionId,-isOpta,-tournamentRegionCode,-regionCode,-teamId,-teamRegionName,-seasonName,-seasonId,-ranking)
+
+
